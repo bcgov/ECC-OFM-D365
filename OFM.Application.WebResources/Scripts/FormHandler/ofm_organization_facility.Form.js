c71eb42e844843db8265a4b0f13ca50705106d45
Hoang <hoang.le@cgi.com>
2023-10-13 17:03:31 -0700

    Add new WebResource project & templates

---
+++ OFM.Application.WebResources/Scripts/FormHandler/ofm_organization_facility.Form.js
@@ -0,0 +1,37 @@
+﻿"use strict";
+
+var OFM = OFM || {};
+OFM.OrganizationFacility = OFM.OrganizationFacility || {};
+OFM.OrganizationFacility.Form = OFM.OrganizationFacility.Form || {};
+
+//Formload logic starts here
+OFM.OrganizationFacility.Form = {
+    onLoad: function (executionContext) {
+        debugger;
+        let formContext = executionContext.getFormContext();
+        switch (formContext.ui.getFormType()) {
+            case 0: //undefined
+                break;
+            case 1: //Create/QuickCreate
+
+            case 2: // update                           
+                this.getTypeOfForm();
+                break;
+            case 3: //readonly
+                break;
+            case 4: //disable
+                break;
+            case 6: //bulkedit
+                break;
+        }
+    },
+
+    //A function called on save
+    onSave: function (executionContext) {
+
+    },
+
+    getTypeOfForm: function () {
+
+    }
+};
---
+++ OFM.Application.WebResources/Scripts/Ribbon/ofm_business_bceid.Ribbon.js
@@ -0,0 +1,13 @@
+﻿"use strict";
+
+var OFM = OFM || {};
+OFM.BusinessBCeID = OFM.BusinessBCeID || {};
+OFM.BusinessBCeID.Ribbon = OFM.BusinessBCeID.Ribbon || {};
+
+OFM.BusinessBCeID.Ribbon = {
+    // description
+    anewFunction: function (primaryControl, selectedIds) {
+        var formContext = primaryControl;
+       
+    }
+}
