39ea81c88b97c33303ebd19df9388ad3e5f5988f
Richard <Richard.ma@cgi.com>
2024-03-27 09:54:02 -0700

    Added PCM Review workflow activity

---
+++ OFM.Infrastructure.CustomWorkflowActivities/PCMReview/GetNotCompletePCMNum.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace OFM.Infrastructure.CustomWorkflowActivities.PCMReview
+{
+    public sealed class GetNotCompletePCMNum : CodeActivity
+    {
+        [Output("Not Complete PCM Number")]
+        public OutArgument<int> pcmNum { get; set; }
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            ITracingService tracingService = executionContext.GetExtension<ITracingService>();
+            IWorkflowContext context = executionContext.GetExtension<IWorkflowContext>();
+            //Create an Organization Service
+            IOrganizationServiceFactory serviceFactory = executionContext.GetExtension<IOrganizationServiceFactory>();
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.InitiatingUserId);
+            var recordId = context.PrimaryEntityId;
+            tracingService.Trace("Begin get not complete PCM Review records, recordId: {0}", recordId);
+            try
+            {
+                // StatusReason 4 Pass, 5 Fail
+                var fetchXml = $@"<?xml version=""1.0"" encoding=""utf-16""?>
+                        <fetch>
+                          <entity name=""ofm_pcm_review"">
+                            <attribute name=""createdon"" />
+                            <attribute name=""ofm_application"" />
+                            <attribute name=""ofm_name"" />
+                            <attribute name=""ofm_pcm_reviewid"" />
+                            <attribute name=""ofm_reason"" />
+                            <attribute name=""statuscode"" />
+                            <filter>
+                              <condition attribute=""ofm_application"" operator=""eq"" value=""{recordId}"" />
+                              <condition attribute=""statuscode"" operator=""ne"" value=""4"" />
+                              <condition attribute=""statuscode"" operator=""ne"" value=""5"" />
+                            </filter>
+                          </entity>
+                        </fetch>";
+                EntityCollection pcmReviews = service.RetrieveMultiple(new FetchExpression(fetchXml));
+                tracingService.Trace("PCM Records number which is not complete:" + pcmReviews.Entities.Count);
+                this.pcmNum.Set(executionContext, pcmReviews.Entities.Count);
+                tracingService.Trace("Workflow activity end.");
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidWorkflowException("Exeception in Custom Workflow -" + ex.Message + ex.InnerException);
+            }
+        }
+    }
+}
