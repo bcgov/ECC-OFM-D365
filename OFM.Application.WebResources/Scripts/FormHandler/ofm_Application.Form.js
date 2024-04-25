ddbeb63474ef6c02927ed5dc29f52a2b06e44b6c
Supreet Kaur <supreet.kaur@cgi.com>
2024-04-25 15:47:03 -0600

    Changes to user role name instead of roleid

--- OFM.Application.WebResources/Scripts/FormHandler/ofm_Application.Form.js
+++ OFM.Application.WebResources/Scripts/FormHandler/ofm_Application.Form.js
@@ -31,10 +31,12 @@ OFM.Application.Form = {
                 this.licenceCheck(executionContext);
                 this.showBanner(executionContext);
                 this.lockStatusReason(executionContext);
+                this.hideVerificationTab(executionContext);
                 break;
 
             case 3: //readonly
                 this.showBanner(executionContext);
+                this.hideVerificationTab(executionContext);
                 break;
 
             case 4: //disable
@@ -367,11 +369,10 @@ OFM.Application.Form = {
     hideVerificationTab: function (executionContext) {
         debugger;
         var formContext = executionContext.getFormContext();
-        var roleId = "ff3b00a4-0f8d-4519-8169-b3dc715992f4";
         var userRoles = Xrm.Utility.getGlobalContext().userSettings.roles;
         if (userRoles.getLength() > 1) { }
 
-        else if (userRoles.get()[0].id == roleId) {
+        else if (userRoles.get()[0].name == "OFM - Read Only") {
             formContext.ui.tabs.get("tab_9").setVisible(false);
         }
     }
