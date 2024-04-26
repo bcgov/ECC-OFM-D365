6239ce803fb1a7102a6fc9d8d6505ae9893c7af5
Lo <Jason.Lo@gov.bc.ca>
2024-04-26 12:33:24 -0700

    createdby filter function changes

--- OFM.Application.WebResources/Scripts/FormHandler/ofm_Application.Form.js
+++ OFM.Application.WebResources/Scripts/FormHandler/ofm_Application.Form.js
@@ -22,6 +22,7 @@ OFM.Application.Form = {
                 this.filterExpenseAuthorityLookup(executionContext);
                 this.UpdateOrganizationdetails(executionContext);
                 this.showBanner(executionContext);
+                this.filterCreatedBySPLookup(executionContext);
                 break;
 
             case 2: // update
@@ -31,6 +32,7 @@ OFM.Application.Form = {
                 this.licenceCheck(executionContext);
                 this.showBanner(executionContext);
                 this.lockStatusReason(executionContext);
+                this.filterCreatedBySPLookup(executionContext);
                 break;
 
             case 3: //readonly
@@ -181,15 +183,60 @@ OFM.Application.Form = {
         // perform operations on record retrieval
     },
          //A function called to filter active contacts associated to organization (lookup on Organization form)
-         filterCreatedBySPLookup: function (executionContext) {
+        //  filterCreatedBySPLookup: function (executionContext) {
+        //     //debugger;
+        //     var formContext = executionContext.getFormContext();
+        //     var formLabel = formContext.ui.formSelector.getCurrentItem().getLabel();	    // get current form's label
+        //     var createdby = formContext.getAttribute("ofm_createdby").getValue();
+        //     var createdbyid;
+        //     if (createdby != null) {
+        //         createdbyid = createdby[0].id;
+    
+        //         var viewId = "{00000000-0000-0000-0000-000000000090}";
+        //         var entity = "contact";
+        //         var ViewDisplayName = "createdby Contacts";
+        //         var fetchXML = "<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='true'>" +
+        //             "<entity name='contact'>" +
+        //             "<attribute name='fullname' />" +
+        //             "<attribute name='ccof_username' />" +
+        //             "<attribute name='parentcustomerid' />" +
+        //             "<attribute name='emailaddress1' />" +
+        //             "<attribute name='contactid' />" +
+        //             "<order attribute='fullname' descending='false' />" +
+        //             "<link-entity name='ofm_bceid_facility' from='ofm_bceid' to='contactid' link-type='inner' alias='an'>" +
+        //             "<filter type='and'>" +
+        //             "<condition attribute='ofm_createdby' operator='eq'  uitype='account' value='" + createdbyid + "'/>" +
+        //             "<condition attribute='ofm_portal_access' operator='eq' value='1' />" +
+        //             "</filter></link-entity></entity></fetch>";
+
+
+    
+    
+        //         var layout = "<grid name='resultset' jump='fullname' select='1' icon='1' preview='1'>" +
+        //             "<row name = 'result' id = 'contactid' >" +
+        //             "<cell name='fullname' width='300' />" +
+        //             "<cell name='ccof_username' width='125' />" +
+        //             "<cell name='emailaddress1' width='150' />" +
+        //             "<cell name='parentcustomerid' width='150' />" +
+        //             "</row></grid>";
+    
+        //         formContext.getControl("ofm_createdby").addCustomView(viewId, entity, ViewDisplayName, fetchXML, layout, true);
+    
+        //     }
+        //     else {
+        //         formContext.getAttribute("ofm_contact").setValue(null);
+    
+        //     }
+        //     // perform operations on record retrieval
+        // },
+        filterCreatedBySPLookup: function (executionContext) {
             //debugger;
             var formContext = executionContext.getFormContext();
-            var formLabel = formContext.ui.formSelector.getCurrentItem().getLabel();	    // get current form's label
             var createdby = formContext.getAttribute("ofm_createdby").getValue();
             var createdbyid;
             if (createdby != null) {
                 createdbyid = createdby[0].id;
-    
+        
                 var viewId = "{00000000-0000-0000-0000-000000000090}";
                 var entity = "contact";
                 var ViewDisplayName = "createdby Contacts";
@@ -201,13 +248,12 @@ OFM.Application.Form = {
                     "<attribute name='emailaddress1' />" +
                     "<attribute name='contactid' />" +
                     "<order attribute='fullname' descending='false' />" +
-                    "<link-entity name='ofm_bceid_facility' from='ofm_bceid' to='contactid' link-type='inner' alias='an'>" +
+                    "<link-entity name='ofm_bceid_facility' from='facilityid' to='parentcustomerid' link-type='inner' alias='an'>" +
                     "<filter type='and'>" +
-                    "<condition attribute='ofm_createdby' operator='eq'  uitype='account' value='" + createdbyid + "'/>" +
+                    "<condition attribute='createdby' operator='eq' value='" + createdbyid + "'/>" +
                     "<condition attribute='ofm_portal_access' operator='eq' value='1' />" +
                     "</filter></link-entity></entity></fetch>";
-    
-    
+        
                 var layout = "<grid name='resultset' jump='fullname' select='1' icon='1' preview='1'>" +
                     "<row name = 'result' id = 'contactid' >" +
                     "<cell name='fullname' width='300' />" +
@@ -215,16 +261,15 @@ OFM.Application.Form = {
                     "<cell name='emailaddress1' width='150' />" +
                     "<cell name='parentcustomerid' width='150' />" +
                     "</row></grid>";
-    
+        
                 formContext.getControl("ofm_createdby").addCustomView(viewId, entity, ViewDisplayName, fetchXML, layout, true);
-    
             }
             else {
                 formContext.getAttribute("ofm_contact").setValue(null);
-    
             }
             // perform operations on record retrieval
         },
+        
 
     // function to validate seconday and primary contact
     validateSecondaryContact: function (executionContext) {
