282c761654a290decca62b745b1c627812ec6f77
hoang.le <hoang.le@cgi.com>
2024-05-29 07:09:08 -0700

    Correct Configmap script to include timestamp and formatting

--- tools/config/update-configmap.sh
+++ tools/config/update-configmap.sh
@@ -36,7 +36,7 @@ D365_LOG_LEVEL=$(cat << JSON
   "Default": "Warning",
   "OFM.Portal.ProviderProfile": "Error",
   "OFM.D365.Process": "Warning",
-  "OFM.D365.Batch": "Error",
+  "OFM.D365.Batch": "Warning",
   "Microsoft.AspNetCore": "Warning"
 }
 JSON
@@ -53,6 +53,23 @@ readonly D365_EMAIL_SAFE_LIST_ENABLE
 D365_CONFIGURATION=$(jq << JSON
 {
   "Logging": $D365_LOG_LEVEL,
+  "Console": {
+    "FormatterName": "simple",
+    "FormatterOptions": {
+      "SingleLine": true,
+      "IncludeScopes": true,
+      "TimestampFormat": "yyyy-MM-ddTHH:mm:ss",
+      "UseUtcTimestamp": false,
+      "JsonWriterOptions": {
+        "Indented": true
+      }
+    }
+  },
+  "Debug": {
+    "LogLevel": {
+      "Default": "Critical"
+    }
+  }
   "AllowedHosts": "*",
   "AppSettings": {
     "PageSize": 50,
