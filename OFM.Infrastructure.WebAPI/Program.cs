c9811e4adf7cc951d6b880b940067829bfdb81e6
hoang.le <hoang.le@cgi.com>
2024-06-14 17:08:56 -0700

    minor refactoring

--- OFM.Infrastructure.WebAPI/Program.cs
+++ OFM.Infrastructure.WebAPI/Program.cs
@@ -57,6 +57,7 @@ services.AddScoped<ID365ProcessProvider, P205SendNotificationProvider>();
 services.AddScoped<ID365ProcessProvider, P210CreateFundingNotificationProvider>();
 services.AddScoped<ID365ProcessProvider, P215SendSupplementaryRemindersProvider>();
 services.AddScoped<ID365ProcessProvider, P220CreateSuppletaryRemindersProvider>();
+services.AddScoped<ID365ProcessProvider, P225SendCertificateNotificationProvider>();
 services.AddScoped<ID365ProcessProvider, P300BaseFundingProvider>();
 services.AddScoped<ID365ProcessProvider, P305SupplementaryFundingProvider>();
 services.AddScoped<ID365ProcessProvider, P310CalculateDefaultAllocationProvider>();
@@ -66,10 +67,10 @@ services.AddScoped<ID365ProcessProvider, P500SendPaymentRequestProvider>();
 services.AddScoped<ID365ProcessProvider, P505GeneratePaymentLinesProvider>();
 services.AddScoped<ID365ProcessProvider, P510ReadPaymentResponseProvider>();
 services.AddScoped<ID365ProcessProvider, P600CloneFundingReportResponse>();
-services.AddScoped<ID365ProcessProvider, P610CreateQuestionProvider>();
 services.AddScoped<ID365ProcessProvider, P605CloseDuedReportsProvider>();
+services.AddScoped<ID365ProcessProvider, P610CreateQuestionProvider>();
 services.AddScoped<ID365ProcessProvider, P615CreateMonthlyReportProvider>();
-services.AddScoped<ID365ProcessProvider, P225SendECENotificationProvider>();
+
 services.AddScoped<D365Email>();
 services.AddScoped<ID365BackgroundProcessHandler, D365BackgroundProcessHandler>();
 
--- OFM.Infrastructure.WebAPI/Services/Processes/Emails/P225SendCertificateNotificationProvider.cs
+++ OFM.Infrastructure.WebAPI/Services/Processes/Emails/P225SendCertificateNotificationProvider.cs
@@ -3,47 +3,35 @@ using OFM.Infrastructure.WebAPI.Extensions;
 using OFM.Infrastructure.WebAPI.Models;
 using OFM.Infrastructure.WebAPI.Services.AppUsers;
 using OFM.Infrastructure.WebAPI.Services.D365WebApi;
-using System.Globalization;
 using System.Text.Json.Nodes;
 using System.Text.Json;
-using OFM.Infrastructure.WebAPI.Messages;
 using System.Net;
 using OFM.Infrastructure.WebAPI.Services.Processes.Fundings;
 using OFM.Infrastructure.WebAPI.Models.Fundings;
 
 
-namespace OFM.Infrastructure.WebAPI.Services.Processes.Emails 
+namespace OFM.Infrastructure.WebAPI.Services.Processes.Emails
 {
-      public class P225SendECENotificationProvider : ID365ProcessProvider
-        {
-        private readonly IEmailRepository _emailRepository;
-        private readonly ID365AppUserService _appUserService;
-        private readonly ID365WebApiService _d365webapiservice;
-        private readonly ILogger _logger ;
-        private readonly TimeProvider _timeProvider ;
-        private readonly NotificationSettings _notificationSettings ;
+    public class P225SendCertificateNotificationProvider(IOptionsSnapshot<NotificationSettings> notificationSettings, ID365AppUserService appUserService, ID365WebApiService d365WebApiService, ILoggerFactory loggerFactory, TimeProvider timeProvider, IEmailRepository emailRepository) : ID365ProcessProvider
+    {
+        private readonly IEmailRepository _emailRepository = emailRepository;
+        private readonly ID365AppUserService _appUserService = appUserService;
+        private readonly ID365WebApiService _d365webapiservice = d365WebApiService;
+        private readonly ILogger _logger = loggerFactory.CreateLogger(LogCategory.Process);
+        private readonly TimeProvider _timeProvider = timeProvider;
+        private readonly NotificationSettings _notificationSettings = notificationSettings.Value;
         private ProcessParameter? _processParams;
-     
-        public P225SendECENotificationProvider(IOptionsSnapshot<NotificationSettings> notificationSettings, ID365AppUserService appUserService, ID365WebApiService d365WebApiService, ILoggerFactory loggerFactory, TimeProvider timeProvider, IFundingRepository fundingRepository, IEmailRepository emailRepository)
-        {
-            _notificationSettings = notificationSettings.Value;
-            _appUserService = appUserService;
-            _d365webapiservice = d365WebApiService;
-             _emailRepository = emailRepository;
-            _logger = loggerFactory.CreateLogger(LogCategory.Process);
-            _timeProvider = timeProvider;
-        }
 
         public short ProcessId => Setup.Process.Emails.CreateECENotificationsId;
         public string ProcessName => Setup.Process.Emails.CreateECENotificationsName;
 
         //To retrieve submitted application.
         private string RetrieveApplications
+        {
+            get
             {
-                get
-                {
-                    // Note: FetchXMl limit is 5000 records per request
-                    var fetchXml = $"""
+                // Note: FetchXMl limit is 5000 records per request
+                var fetchXml = $"""
                 <fetch version="1.0" output-format="xml-platform" mapping="logical" distinct="false">
                   <entity name="ofm_provider_employee">
                     <attribute name="ofm_provider_employeeid" />
@@ -75,62 +63,61 @@ namespace OFM.Infrastructure.WebAPI.Services.Processes.Emails
                          ofm_provider_employees?fetchXml={WebUtility.UrlEncode(fetchXml)}
                          """;
                 return requestUri.CleanCRLF();
-                }
             }
+        }
 
+        public async Task<ProcessData> GetDataAsync()
+        {
+            _logger.LogDebug(CustomLogEvent.Process, "Calling GetSupplementaryApplicationDataAsync");
 
-            public async Task<ProcessData> GetDataAsync()
+            var response = await _d365webapiservice.SendRetrieveRequestAsync(_appUserService.AZSystemAppUser, RetrieveApplications, formatted: true, isProcess: true);
+
+            if (!response.IsSuccessStatusCode)
             {
-                _logger.LogDebug(CustomLogEvent.Process, "Calling GetSupplementaryApplicationDataAsync");
+                var responseBody = await response.Content.ReadAsStringAsync();
+                _logger.LogError(CustomLogEvent.Process, "Failed to query supplementary applications to update with the server error {responseBody}", responseBody.CleanLog());
 
-                var response = await _d365webapiservice.SendRetrieveRequestAsync(_appUserService.AZSystemAppUser, RetrieveApplications, formatted:true,isProcess: true);
-
-                if (!response.IsSuccessStatusCode)
-                {
-                    var responseBody = await response.Content.ReadAsStringAsync();
-                    _logger.LogError(CustomLogEvent.Process, "Failed to query supplementary applications to update with the server error {responseBody}", responseBody.CleanLog());
-
-                    return await Task.FromResult(new ProcessData(string.Empty));
-                }
-
-                var jsonObject = await response.Content.ReadFromJsonAsync<JsonObject>();
-
-                JsonNode d365Result = string.Empty;
-                if (jsonObject?.TryGetPropertyValue("value", out var currentValue) == true)
-                {
-                    if (currentValue?.AsArray().Count == 0)
-                    {
-                        _logger.LogInformation(CustomLogEvent.Process, "No supplementary applications found with query {requestUri}", RetrieveApplications.CleanLog());
-                    }
-                    d365Result = currentValue!;
-                }
-
-                _logger.LogDebug(CustomLogEvent.Process, "Query Result {queryResult}", d365Result.ToString().CleanLog());
-
-                return await Task.FromResult(new ProcessData(d365Result));
+                return await Task.FromResult(new ProcessData(string.Empty));
             }
+
+            var jsonObject = await response.Content.ReadFromJsonAsync<JsonObject>();
+
+            JsonNode d365Result = string.Empty;
+            if (jsonObject?.TryGetPropertyValue("value", out var currentValue) == true)
+            {
+                if (currentValue?.AsArray().Count == 0)
+                {
+                    _logger.LogInformation(CustomLogEvent.Process, "No supplementary applications found with query {requestUri}", RetrieveApplications.CleanLog());
+                }
+                d365Result = currentValue!;
+            }
+
+            _logger.LogDebug(CustomLogEvent.Process, "Query Result {queryResult}", d365Result.ToString().CleanLog());
+
+            return await Task.FromResult(new ProcessData(d365Result));
+        }
 
         public async Task<JsonObject> RunProcessAsync(ID365AppUserService appUserService, ID365WebApiService d365WebApiService, ProcessParameter processParams)
         {
-          _processParams = processParams;
-           var localData = await GetDataAsync();
+            _processParams = processParams;
+            var localData = await GetDataAsync();
+
             string staffdetails = "<ul>";
             List<ProviderStaff> _staffs = new List<ProviderStaff>();
             IEnumerable<D365CommunicationType> _communicationType = await _emailRepository!.LoadCommunicationTypeAsync();
 
-          var  _informationCommunicationType = _communicationType.Where(c => c.ofm_communication_type_number == _notificationSettings.CommunicationTypes.ActionRequired)
-                                                                       .Select(s => s.ofm_communication_typeid).FirstOrDefault();
-         _staffs = string.IsNullOrEmpty(localData.Data.ToString()) ? null : System.Text.Json.JsonSerializer.Deserialize<List<ProviderStaff>>(localData.Data.ToString());
-            
+            var _informationCommunicationType = _communicationType.Where(c => c.ofm_communication_type_number == _notificationSettings.CommunicationTypes.ActionRequired)
+                                                                         .Select(s => s.ofm_communication_typeid).FirstOrDefault();
 
-          
+            _staffs = string.IsNullOrEmpty(localData.Data.ToString()) ? null : System.Text.Json.JsonSerializer.Deserialize<List<ProviderStaff>>(localData.Data.ToString());
+
             #region Create the ECE email notifications 
             // Get template details to create emails.
-            if(_staffs?.Count >0)
-           {
+            if (_staffs?.Count > 0)
+            {
                 foreach (var staff in _staffs)
                 {
-                    staffdetails += "<li>"+staff.Initials + " : " + staff.CertificateNumber + "</li><br/>";
+                    staffdetails += "<li>" + staff.Initials + " : " + staff.CertificateNumber + "</li><br/>";
                 }
                 staffdetails += "</ul>";
                 var localDataTemplate = await _emailRepository.GetTemplateDataAsync(_notificationSettings.EmailTemplates.First(t => t.TemplateNumber == 226).TemplateNumber);
@@ -139,14 +126,15 @@ namespace OFM.Infrastructure.WebAPI.Services.Processes.Emails
                 string? subject = templateobj?.title;
                 string? emaildescription = templateobj?.safehtml;
                 emaildescription = emaildescription?.Replace("{Application_No}", _staffs?[0].ApplicationId);
-                emaildescription = emaildescription?.Replace("{Provider_Name}", _staffs?[0].ProviderName);              
+                emaildescription = emaildescription?.Replace("{Provider_Name}", _staffs?[0].ProviderName);
                 emaildescription = emaildescription?.Replace("{Staff}", staffdetails);
                 List<Guid> recipientsList = new List<Guid>();
                 recipientsList.Add(_staffs[0].ProviderId);
-                 await _emailRepository.CreateAndUpdateEmail(subject, emaildescription, recipientsList, _processParams.Notification.SenderId, _informationCommunicationType, appUserService, d365WebApiService, 225);
 
-               
+                await _emailRepository.CreateAndUpdateEmail(subject, emaildescription, recipientsList, _processParams.Notification.SenderId, _informationCommunicationType, appUserService, d365WebApiService, 225);
+
             }
+
             return ProcessResult.Completed(ProcessId).SimpleProcessResult;
 
             #endregion
