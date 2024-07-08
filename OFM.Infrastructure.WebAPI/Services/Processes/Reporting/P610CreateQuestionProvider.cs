b271de56a3d473bd75ebbce9e15f237fe01ebaa6
Supreet Kaur <supreet.kaur@cgi.com>
2024-07-08 13:00:10 -0600

    minor change on the query to get latest version

--- OFM.Infrastructure.WebAPI/Services/Processes/Reporting/P610CreateQuestionProvider.cs
+++ OFM.Infrastructure.WebAPI/Services/Processes/Reporting/P610CreateQuestionProvider.cs
@@ -259,7 +259,7 @@ public class P610CreateQuestionProvider : ID365ProcessProvider
                     """;
 
             var requestUri = $"""
-                         ofm_questions?fetchXml={WebUtility.UrlEncode(fetchXml)}
+                         ofm_surveies?fetchXml={WebUtility.UrlEncode(fetchXml)}
                          """;
 
             return requestUri;
