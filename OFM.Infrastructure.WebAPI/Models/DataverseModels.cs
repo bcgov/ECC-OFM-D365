643970c384891dc7413d6ca25ee6597937e62716
hoang.le <hoang.le@cgi.com>
2024-07-26 09:02:09 -0700

    Add paymentmethod to D365PaymentLine

--- OFM.Infrastructure.WebAPI/Models/DataverseModels.cs
+++ OFM.Infrastructure.WebAPI/Models/DataverseModels.cs
@@ -396,12 +396,12 @@ public class D365PaymentLine : ofm_payment
     public new Facility? ofm_facility { get; set; }
     public new Application? ofm_application { get; set; }
     public new Funding? ofm_funding { get; set; }
-    public string _ofm_funding_value { get; set; } = string.Empty;
     public new D365FiscalYear? ofm_fiscal_year { get; set; }
     public new string ofm_invoice_number { get; set; } = string.Empty;
     public new string ofm_siteid { get; set; } = string.Empty;
     public new string ofm_supplierid { get; set; } = string.Empty;
-    public string _ofm_regardingid_value { get; set; } = string.Empty;
+    public new string ofm_payment_method { get; set; } = string.Empty;
+    public string _ofm_regardingid_value { get; set; } = string.Empty; // ToDo: a workaround for polymorphic lookup
 }
 
 public class ProviderStaff
