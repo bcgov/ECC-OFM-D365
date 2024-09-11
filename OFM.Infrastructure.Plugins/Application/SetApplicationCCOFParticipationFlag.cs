24d04d41f8cc9a78184301bd1a4d438de0077811
yiyiwang-cgi <yiyi.wang@cgi.com>
2024-09-11 15:23:40 -0700

    Added Plugin for Setting Applicaion CCOF Participation Flag

---
+++ OFM.Infrastructure.Plugins/Application/SetApplicationCCOFParticipationFlag.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECC.Core.DataContext;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Messages;
+using System.IdentityModel.Metadata;
+
+namespace OFM.Infrastructure.Plugins.Application
+{
+    public class SetApplicationCCOFParticipationFlag : PluginBase
+    {
+        public SetApplicationCCOFParticipationFlag(string unsecureConfiguration, string secureConfiguration)
+             : base(typeof(SetApplicationCCOFParticipationFlag))
+        {
+            // TODO: Implement your custom configuration handling
+            // https://docs.microsoft.com/powerapps/developer/common-data-service/register-plug-in#set-configuration-data
+        }
+
+        protected override void ExecuteDataversePlugin(ILocalPluginContext localPluginContext)
+        {
+            if (localPluginContext == null)
+            {
+                throw new InvalidPluginExecutionException(nameof(localPluginContext), new Dictionary<string, string>() { ["failedRecordId"] = localPluginContext.Target.Id.ToString() });
+            }
+
+            localPluginContext.Trace("Start SetApplicationCCOFParticipationFlag Plug-in");
+
+            if (localPluginContext.Target.Contains(ofm_application.Fields.statuscode))
+            {
+                using (var crmContext = new DataverseContext(localPluginContext.PluginUserService))
+                {
+                    
+                   
+                    //Get facility program start date
+                    var application = localPluginContext.PluginUserService.Retrieve(ofm_application.EntityLogicalName, localPluginContext.Target.Id, new ColumnSet(true));
+
+                    var status = application.GetAttributeValue<OptionSetValue>(ofm_application.Fields.statuscode);
+
+                    localPluginContext.Trace($"status {status.Value}");
+
+                    if (status.Value == (int) ofm_application_StatusCode.Submitted)
+                    {
+                        EntityReference facility = application.GetAttributeValue<EntityReference>(ofm_application.Fields.ofm_facility);
+                        if (facility != null)
+                        {
+                            var programStartDate = localPluginContext.PluginUserService.Retrieve(facility.LogicalName, facility.Id, new ColumnSet(true)).GetAttributeValue<DateTime>(Account.Fields.ofm_program_start_date);
+                            localPluginContext.Trace($"programStartDate {programStartDate}");
+
+                            //Compare the application submission time and facility program start date
+                            var applicationSubmittedOn = application.GetAttributeValue<DateTime>(ofm_application.Fields.ofm_summary_submittedon);
+
+                            var  entity = new ofm_application
+                            {
+                                Id = localPluginContext.Target.Id
+                            };
+
+                            if (applicationSubmittedOn >= programStartDate.AddYears(1))
+                            {
+                                //Set CCOF Participation flag to true
+                                entity["ofm_system_ccof_tdad_participation"] = true;
+                                UpdateRequest updateRequest = new UpdateRequest { Target = entity };
+                                crmContext.Execute(updateRequest);
+
+                            }
+                            else
+                            {
+                                //Set CCOF Participation flag to false
+                                entity["ofm_system_ccof_tdad_participation"] = false;
+                                UpdateRequest updateRequest = new UpdateRequest { Target = entity };
+                                crmContext.Execute(updateRequest);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
