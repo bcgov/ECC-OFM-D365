32491198ade076b22c9f71e92f4e5875f0ae720a
yiyiwang-cgi <yiyi.wang@cgi.com>
2024-09-19 08:29:14 -0700

    Update with facility start date

--- OFM.Infrastructure.Plugins/Application/SetApplicationCCOFParticipationFlag.cs
+++ OFM.Infrastructure.Plugins/Application/SetApplicationCCOFParticipationFlag.cs
@@ -47,8 +47,8 @@ namespace OFM.Infrastructure.Plugins.Application
                         EntityReference facility = application.GetAttributeValue<EntityReference>(ofm_application.Fields.ofm_facility);
                         if (facility != null)
                         {
-                            var programStartDate = localPluginContext.PluginUserService.Retrieve(facility.LogicalName, facility.Id, new ColumnSet(true)).GetAttributeValue<DateTime>(Account.Fields.ofm_program_start_date);
-                            localPluginContext.Trace($"programStartDate {programStartDate}");
+                            var facilityStartDate = localPluginContext.PluginUserService.Retrieve(facility.LogicalName, facility.Id, new ColumnSet(true)).GetAttributeValue<DateTime>(Account.Fields.ccof_facilitystartdate);
+                            localPluginContext.Trace($"facilityStartDate {facilityStartDate}");
 
                             //Compare the application submission time and facility program start date
                             var applicationSubmittedOn = application.GetAttributeValue<DateTime>(ofm_application.Fields.ofm_summary_submittedon);
@@ -58,21 +58,10 @@ namespace OFM.Infrastructure.Plugins.Application
                                 Id = localPluginContext.Target.Id
                             };
 
-                            if (applicationSubmittedOn >= programStartDate.AddYears(1))
-                            {
-                                //Set CCOF Participation flag to true
-                                entity["ofm_system_ccof_tdad_participation"] = true;
-                                UpdateRequest updateRequest = new UpdateRequest { Target = entity };
-                                crmContext.Execute(updateRequest);
+                            entity["ofm_system_ccof_tdad_participation"] = applicationSubmittedOn >= facilityStartDate.AddYears(1);
 
-                            }
-                            else
-                            {
-                                //Set CCOF Participation flag to false
-                                entity["ofm_system_ccof_tdad_participation"] = false;
-                                UpdateRequest updateRequest = new UpdateRequest { Target = entity };
-                                crmContext.Execute(updateRequest);
-                            }
+                            UpdateRequest updateRequest = new UpdateRequest { Target = entity };
+                            crmContext.Execute(updateRequest);
                         }
                     }
                 }
