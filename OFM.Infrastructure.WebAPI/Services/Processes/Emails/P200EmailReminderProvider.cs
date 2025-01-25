dfefbc5b785e69d4cc20f12b3aa75b7b557209e5
PhillipECC <Phillip.Fong@gov.bc.ca>
2025-01-24 16:46:31 -0800

    2 Way Communication for Assistance Request

--- OFM.Infrastructure.WebAPI/Services/Processes/Emails/P200EmailReminderProvider.cs
+++ OFM.Infrastructure.WebAPI/Services/Processes/Emails/P200EmailReminderProvider.cs
@@ -20,9 +20,11 @@ public class P200EmailReminderProvider : ID365ProcessProvider
     private readonly ILogger _logger;
     private readonly TimeProvider _timeProvider;
     private ProcessData? _data;
+    private ProcessData? _assistanceRequestdata;
     private string[] _activeCommunicationTypes = [];
     private string[] _communicationTypesForUnreadReminders = [];
     private string _requestUri = string.Empty;
+    private string _assistanceRequestUri = string.Empty;
 
     public P200EmailReminderProvider(IOptionsSnapshot<NotificationSettings> notificationSettings, IOptionsSnapshot<D365AuthSettings> d365AuthSettings, IEmailRepository emailRepository, ID365AppUserService appUserService, ID365WebApiService d365WebApiService, ILoggerFactory loggerFactory, TimeProvider timeProvider)
     {
@@ -104,14 +106,64 @@ public class P200EmailReminderProvider : ID365ProcessProvider
             return _requestUri;
         }
     }
+    public string AssistanceRequestUri
+    {
+        get
+        {
+            // Note: FetchXMl limit is 5000 records per request
+            // Use Paging Cookie for large datasets
+            // Use exact filters to reduce the matching data and enhance performance
+
+            if (string.IsNullOrEmpty(_assistanceRequestUri))
+            {
+
+                //for reference only
+                var fetchXml = $"""
+                                <fetch>
+                                  <entity name="ofm_assistance_request">
+                                    <attribute name="ofm_assistance_requestid" />
+                                    <attribute name="ofm_name" />
+                                    <attribute name="ofm_subject" />
+                                    <attribute name="ofm_submission_time" />
+                                    <attribute name="ofm_contact" />
+                                    <attribute name="ofm_is_read" />
+                                    <filter>
+                                      <condition attribute="ofm_submission_time" operator="last-x-days" value="30" />
+                                      <condition attribute="ofm_contact_method" operator="eq" value="1" />
+                                      <condition attribute="ofm_contact" operator="not-null" value="" />
+                                      <condition attribute="statecode" operator="eq" value="0" />
+                                      <condition attribute="ofm_is_read" operator="eq" value="0" />
+                                      <filter type="or">
+                                        <condition attribute="statuscode" operator="eq" value="3" />
+                                        <condition attribute="statuscode" operator="eq" value="4" />
+                                      </filter>
+                                    </filter>
+                                    <link-entity name="contact" from="contactid" to="ofm_contact" link-type="inner" alias="contact">
+                                      <attribute name="emailaddress1"" />
+                                    </link-entity>
+                                  </entity>
+                                </fetch>
+                                """;
+
+                var assistancerequestUri = $"""                                
+                                ofm_assistance_requests?$select=ofm_assistance_requestid,ofm_name,ofm_subject,ofm_submission_time,_ofm_contact_value,ofm_is_read&$expand=ofm_contact($select=emailaddress1)
+                                &$filter=(Microsoft.Dynamics.CRM.LastXDays(PropertyName='ofm_submission_time',PropertyValue=30) and ofm_contact_method eq 1 and _ofm_contact_value ne null and statecode eq 0 and ofm_is_read eq false and (statuscode eq 3 or statuscode eq 4)) and (ofm_contact/contactid ne null)
+                                """;
+
+                _assistanceRequestUri = assistancerequestUri.CleanCRLF();
+            }
+
+            return _assistanceRequestUri;
+        }
+    }
 
     public async Task<ProcessData> GetDataAsync()
     {
-        _logger.LogDebug(CustomLogEvent.Process, "Calling GetData of {nameof}", nameof(P200EmailReminderProvider));
+        _logger.LogDebug(CustomLogEvent.Process, "Calling Email GetData of {nameof}", nameof(P200EmailReminderProvider));
 
         if (_data is null)
         {
-            _logger.LogDebug(CustomLogEvent.Process, "Getting due emails with query {requestUri}", RequestUri);
+            _logger.LogDebug(CustomLogEvent.Process, "Getting due emails with query {requestUri}", AssistanceRequestUri);
 
             var response = await _d365webapiservice.SendRetrieveRequestAsync(_appUserService.AZSystemAppUser, RequestUri, isProcess: true);
 
@@ -143,6 +195,44 @@ public class P200EmailReminderProvider : ID365ProcessProvider
         return await Task.FromResult(_data);
     }
 
+    public async Task<ProcessData> GetAssistanceRequestDataAsync()
+    {
+        _logger.LogDebug(CustomLogEvent.Process, "Calling AssisstanceRequestGetData of {nameof}", nameof(P200EmailReminderProvider));
+
+        if (_assistanceRequestdata is null)
+        {
+            _logger.LogDebug(CustomLogEvent.Process, "Getting due Assistance Request with query {assistancerequestUri}", AssistanceRequestUri);
+
+            var response = await _d365webapiservice.SendRetrieveRequestAsync(_appUserService.AZSystemAppUser, AssistanceRequestUri, isProcess: true);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                var responseBody = await response.Content.ReadAsStringAsync();
+                _logger.LogError(CustomLogEvent.Process, "Failed to query Assistance Requests with the server error {responseBody}", responseBody.CleanLog());
+
+                return await Task.FromResult(new ProcessData(string.Empty));
+            }
+
+            var jsonObject = await response.Content.ReadFromJsonAsync<JsonObject>();
+
+            JsonNode d365Result = string.Empty;
+            if (jsonObject?.TryGetPropertyValue("value", out var currentValue) == true)
+            {
+                if (currentValue?.AsArray().Count == 0)
+                {
+                    _logger.LogInformation(CustomLogEvent.Process, "No Assistance Request found with query {assistancerequestUri}", AssistanceRequestUri);
+                }
+                d365Result = currentValue!;
+            }
+
+            _assistanceRequestdata = new ProcessData(d365Result);
+
+            _logger.LogDebug(CustomLogEvent.Process, "Query Result {_data}", _assistanceRequestdata.Data.ToJsonString(Setup.s_writeOptionsForLogs));
+        }
+
+        return await Task.FromResult(_assistanceRequestdata);
+    }
+
     public async Task<JsonObject> RunProcessAsync(ID365AppUserService appUserService, ID365WebApiService d365WebApiService, ProcessParameter processParams)
     {
         IEnumerable<D365CommunicationType> _communicationType = await _emailRepository!.LoadCommunicationTypeAsync();
@@ -170,7 +260,12 @@ public class P200EmailReminderProvider : ID365ProcessProvider
 
         // Send only one email per contact
         var uniqueContacts = dueEmails.SelectMany(e => e.email_activity_parties.Select(ap => new { ap.addressused, ap._partyid_value })).Distinct().ToList();
-        if (uniqueContacts.Count == 0)
+
+        var AssistanceRequestData = await GetAssistanceRequestDataAsync();
+        var serializedAssistanceRequestData = System.Text.Json.JsonSerializer.Deserialize<List<D365AssistanceRequest>>(AssistanceRequestData.Data, Setup.s_writeOptionsForLogs);
+        var dueAssistanceRequests = serializedAssistanceRequestData!.Where(e => AssistanceRequestIsNewAndUnread(e, todayRange) || IsUnreadReminderRequiredAssistanceRequest(e, todayRange)).ToList();
+
+        if (uniqueContacts.Count == 0 && dueAssistanceRequests.Count == 0)
         {
             _logger.LogInformation(CustomLogEvent.Process, "Send email reminders completed. No unique contacts found.");
             return ProcessResult.Completed(ProcessId).SimpleProcessResult;
@@ -191,7 +286,18 @@ public class P200EmailReminderProvider : ID365ProcessProvider
             }
             recipientsList.Add(contactId);
         });
+        dueAssistanceRequests.ForEach(assistanceReq =>
+        {
+            contactId = assistanceReq._ofm_contact_value;
+            if (_notificationSettings.EmailSafeList.Enable &&
+                !_notificationSettings.EmailSafeList.Recipients.Any(x => x.Equals(assistanceReq.emailaddress1?.Trim(';'), StringComparison.CurrentCultureIgnoreCase)))
+            {
+                contactId = _notificationSettings.EmailSafeList.DefaultContactId;
+            }
+            recipientsList.Add(contactId);
+        });
 
+        recipientsList = recipientsList.Distinct().ToList();
         List<HttpRequestMessage> SendEmailFromTemplateRequest = [];
         var templateData = await _emailRepository.GetTemplateDataAsync(_notificationSettings.EmailTemplates.First(t => t.TemplateNumber == 201).TemplateNumber);
         var serializedtemplateData = JsonConvert.DeserializeObject<List<D365Template>>(templateData.Data.ToString());
@@ -272,6 +378,24 @@ public class P200EmailReminderProvider : ID365ProcessProvider
 
         return false;
     }
+    private bool IsUnreadReminderRequiredAssistanceRequest(D365AssistanceRequest assistanceRequest, Range<DateTime> todayRange)
+    {
+        if (assistanceRequest == null)
+            return false;
+
+
+        var firstReminderInDays = _notificationSettings.UnreadEmailOptions.FirstReminderInDays;
+        var secondReminderInDays = _notificationSettings.UnreadEmailOptions.SecondReminderInDays;
+        var thirdReminderInDays = _notificationSettings.UnreadEmailOptions.ThirdReminderInDays;
+        if (todayRange.WithinRange(assistanceRequest.ofm_submission_time.GetValueOrDefault().AddDays(firstReminderInDays)) ||
+        todayRange.WithinRange(assistanceRequest.ofm_submission_time.GetValueOrDefault().AddDays(secondReminderInDays)) ||
+        todayRange.WithinRange(assistanceRequest.ofm_submission_time.GetValueOrDefault().AddDays(thirdReminderInDays)))
+        {
+            return true;
+        }
+
+        return false;
+    }
 
     private bool IsNewAndUnread(D365Email email, Range<DateTime> todayRange)
     {
@@ -280,6 +404,16 @@ public class P200EmailReminderProvider : ID365ProcessProvider
 
         // if the email is created/sent today and is unread
         if (email.lastopenedtime is null && todayRange.WithinRange(email.ofm_sent_on.GetValueOrDefault()))
+            return true;
+
+        return false;
+    }
+
+    private bool AssistanceRequestIsNewAndUnread(D365AssistanceRequest assistanceRequest, Range<DateTime> todayRange)
+    {
+
+        // if the email is created/sent today and is unread
+        if (assistanceRequest.ofm_is_read == false && todayRange.WithinRange(assistanceRequest.ofm_submission_time.GetValueOrDefault()))
             return true;
 
         return false;
